0ec84ee1b3075eeb9152e3e8886905626e5af56f
Karla-Villegas <karlavillegas43@gmail.com>
2021-02-05 23:03:26 -0400

    trabajando en las tareas

--- app/src/main/java/com/example/fire/DashboardFragment.java
+++ app/src/main/java/com/example/fire/DashboardFragment.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.fire.adminSQLite.DaoDB;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +27,12 @@ import android.widget.EditText;
 
 public class DashboardFragment extends Fragment {
 
+    /*RecyclerView recyclerView;
+    ArrayList<Chores> dataholder;*/
+
+        ListView listViewTareas;
+    ArrayList<String> listaInformacion;
+    ArrayList<Chores> listaDeTareas;
 
 
     Button agregar;
@@ -34,8 +47,8 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        // Intent del boton agragar tareas que lleva a la vista registro de tareas
         agregar = (Button) v.findViewById(R.id.BotonAgregar);
-
         agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +57,16 @@ public class DashboardFragment extends Fragment {
 
             }
         });
+
+//        recyclerView = v.findViewById(R.id.recyclerV_Tareas);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        dataholder = new ArrayList<>();
+
+        final DaoDB daoDB = new DaoDB(getApplicationContext());
+
+        listViewTareas = v.findViewById(R.id.listaTareas);
+
+        consultarListaTareas();
 
         return v;
     }
